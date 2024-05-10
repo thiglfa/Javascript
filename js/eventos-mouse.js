607231e5f97ea9e98c643d7eea9c991548cc89cf
thiglfa <thiagllima26@gmail.com>
2024-05-10 11:35:13 -0300

    Click duplo e posição do mouse adicionados

--- js/eventos-mouse.js
+++ js/eventos-mouse.js
@@ -4,7 +4,21 @@ minhaDiv.addEventListener('mouseenter', entrar)
 minhaDiv.addEventListener('mouseout', sair)
 minhaDiv.addEventListener('click', clicar)
 minhaDiv.addEventListener('contextmenu', clickDireito)
+minhaDiv.addEventListener('dblclick', clickDuplo)
+minhaDiv.addEventListener('mousemove', mover)
 
+function mover(e){
+    let x = e.clientX
+    let y = e.clientY
+    let res = document.querySelector('span')
+    res.innerHTML = `Posição X: ${x} e Y: ${y}`
+
+}
+function clickDuplo(){
+    minhaDiv.innerHTML = 'Click Duplo!'
+    minhaDiv.style.backgroundColor = 'red'
+
+}
 function clickDireito(e){
     e.preventDefault()
     minhaDiv.innerHTML = 'Click Direito!'
